ff21abb163fb39b04b9d028da017ecb7fa6fea32
Tricia Swift <tricia.swift8@gmail.com>
2023-11-01 10:45:59 -0500

    Added delete a single rock functionality

--- rockapi/views/rock_view.py
+++ rockapi/views/rock_view.py
@@ -32,6 +32,25 @@ class RockView(ViewSet):
         except Exception as ex:
             return Response({"reason": ex.args[0]}, status=status.HTTP_400_BAD_REQUEST)
 
+    def destroy(self, request, pk=None):
+        """Handle DELETE requests for a single rock
+
+        Returns:
+            Response -- 200, 404, or 500 status code
+        """
+        try:
+            rock = Rock.objects.get(pk=pk)
+            rock.delete()
+            return Response(None, status=status.HTTP_204_NO_CONTENT)
+
+        except Rock.DoesNotExist as ex:
+            return Response({"message": ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+        except Exception as ex:
+            return Response(
+                {"message": ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+
     def list(self, request):
         """Handle GET requests for all rocks
 
